9be6949718a6f5ba60a5549fc6b1907f7ab01ff9
Devlrxxh <seabearvalorant@gmail.com>
2025-07-05 21:39:28 +0200

    chore: missing packet changes in PlayerMoveListener

--- src/main/java/dev/lrxh/blockFlow/listeners/PlayerMoveListener.java
+++ src/main/java/dev/lrxh/blockFlow/listeners/PlayerMoveListener.java
@@ -2,17 +2,23 @@ package dev.lrxh.blockFlow.listeners;
 
 import com.github.retrooper.packetevents.event.PacketListenerAbstract;
 import com.github.retrooper.packetevents.event.PacketReceiveEvent;
+import com.github.retrooper.packetevents.event.PacketSendEvent;
 import com.github.retrooper.packetevents.protocol.packettype.PacketType;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerPosition;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerPositionAndRotation;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerRotation;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityRelativeMove;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityRelativeMoveAndRotation;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerEntityRotation;
 import dev.lrxh.blockFlow.BlockFlow;
 import dev.lrxh.blockFlow.stage.FlowStage;
 import dev.lrxh.blockFlow.stage.impl.FlowBlock;
 import dev.lrxh.blockFlow.stage.impl.FlowPosition;
 import lombok.AllArgsConstructor;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 
 @AllArgsConstructor
@@ -73,7 +79,7 @@ public class PlayerMoveListener extends PacketListenerAbstract {
                         (int) Math.floor(wrapper.getPosition().getZ())
                 );
 
-                if (stage.getBlockDataAt(position).getBlockData().getMaterial() != Material.AIR) {
+               if (stage.getBlockDataAt(position).getBlockData().getMaterial() != Material.AIR) {
                     wrapper.setOnGround(true);
                 }
 
@@ -100,7 +106,7 @@ public class PlayerMoveListener extends PacketListenerAbstract {
                         (int) Math.floor(wrapper.getLocation().getZ())
                 );
 
-                if (stage.getBlockDataAt(position).getBlockData().getMaterial() != Material.AIR) {
+               if (stage.getBlockDataAt(position).getBlockData().getMaterial() != Material.AIR) {
                     wrapper.setOnGround(true);
                 }
 
@@ -127,10 +133,101 @@ public class PlayerMoveListener extends PacketListenerAbstract {
                         (int) Math.floor(wrapper.getLocation().getZ())
                 );
 
-                if (stage.getBlockDataAt(position).getBlockData().getMaterial() != Material.AIR) {
+               if (stage.getBlockDataAt(position).getBlockData().getMaterial() != Material.AIR) {
                     wrapper.setOnGround(true);
                 }
 
+            }
+        }
+
+        packet.markForReEncode(true);
+    }
+
+    @Override
+    public void onPacketSend(PacketSendEvent packet) {
+        Player player = packet.getPlayer();
+
+        if (packet.getPacketType() == PacketType.Play.Server.ENTITY_RELATIVE_MOVE) {
+            WrapperPlayServerEntityRelativeMove wrapper = new WrapperPlayServerEntityRelativeMove(packet);
+
+            for (FlowStage stage : blockFlow.getStages()) {
+                if (!stage.getWatchers().contains(player.getUniqueId())) {
+                    continue;
+                }
+
+                if (!stage.getWorld().equals(player.getWorld())) {
+                    continue;
+                }
+
+                Entity entity = blockFlow.getEntityCache().getEntities().get(wrapper.getEntityId());
+
+                Location location = entity.getLocation();
+
+                FlowPosition position = new FlowPosition(
+                        (int) Math.floor(location.getX()),
+                        (int) Math.floor(location.getY()) - 1,
+                        (int) Math.floor(location.getZ())
+                );
+
+               if (stage.getBlockDataAt(position).getBlockData().getMaterial() != Material.AIR) {
+                    wrapper.setOnGround(true);
+                }
+            }
+        }
+
+        if (packet.getPacketType() == PacketType.Play.Server.ENTITY_ROTATION) {
+            WrapperPlayServerEntityRotation wrapper = new WrapperPlayServerEntityRotation(packet);
+
+            for (FlowStage stage : blockFlow.getStages()) {
+                if (!stage.getWatchers().contains(player.getUniqueId())) {
+                    continue;
+                }
+
+                if (!stage.getWorld().equals(player.getWorld())) {
+                    continue;
+                }
+
+                Entity entity = blockFlow.getEntityCache().getEntities().get(wrapper.getEntityId());
+
+                Location location = entity.getLocation();
+
+                FlowPosition position = new FlowPosition(
+                        (int) Math.floor(location.getX()),
+                        (int) Math.floor(location.getY()) - 1,
+                        (int) Math.floor(location.getZ())
+                );
+
+                if (stage.getBlockDataAt(position).getBlockData().getMaterial() != Material.AIR) {
+                    wrapper.setOnGround(true);
+                }
+            }
+        }
+
+        if (packet.getPacketType() == PacketType.Play.Server.ENTITY_RELATIVE_MOVE_AND_ROTATION) {
+            WrapperPlayServerEntityRelativeMoveAndRotation wrapper = new WrapperPlayServerEntityRelativeMoveAndRotation(packet);
+
+            for (FlowStage stage : blockFlow.getStages()) {
+                if (!stage.getWatchers().contains(player.getUniqueId())) {
+                    continue;
+                }
+
+                if (!stage.getWorld().equals(player.getWorld())) {
+                    continue;
+                }
+
+                Entity entity = blockFlow.getEntityCache().getEntities().get(wrapper.getEntityId());
+
+                Location location = entity.getLocation();
+
+                FlowPosition position = new FlowPosition(
+                        (int) Math.floor(location.getX()),
+                        (int) Math.floor(location.getY()) - 1,
+                        (int) Math.floor(location.getZ())
+                );
+
+               if (stage.getBlockDataAt(position).getBlockData().getMaterial() != Material.AIR) {
+                    wrapper.setOnGround(true);
+                }
             }
         }
 
