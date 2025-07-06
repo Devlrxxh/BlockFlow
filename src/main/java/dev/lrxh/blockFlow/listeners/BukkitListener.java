77a88ecd7126b13a03a7cb8d31b0f33df6f43fee
Devlrxxh <seabearvalorant@gmail.com>
2025-07-06 18:49:32 +0200

    fix: Player drops not getting saved

--- src/main/java/dev/lrxh/blockFlow/listeners/BukkitListener.java
+++ src/main/java/dev/lrxh/blockFlow/listeners/BukkitListener.java
@@ -4,9 +4,11 @@ import dev.lrxh.blockFlow.BlockFlow;
 import dev.lrxh.blockFlow.stage.FlowStage;
 import dev.lrxh.blockFlow.stage.impl.FlowPosition;
 import lombok.AllArgsConstructor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFromToEvent;
+import org.bukkit.event.entity.EntityDropItemEvent;
 
 @AllArgsConstructor
 public class BukkitListener implements Listener {
@@ -22,6 +24,27 @@ public class BukkitListener implements Listener {
             if (flowStage.isPositionInBounds(new FlowPosition(x, y, z))) {
                 event.setCancelled(true);
                 return;
+            }
+        }
+    }
+
+    @EventHandler
+    public void onItemDrop(EntityDropItemEvent event) {
+        if (!(event.getEntity() instanceof Player player)) return;
+
+        int x = event.getItemDrop().getLocation().getBlockX();
+        int y = event.getItemDrop().getLocation().getBlockY();
+        int z = event.getItemDrop().getLocation().getBlockZ();
+
+        for (FlowStage flowStage : blockFlow.getStages()) {
+            if (!flowStage.getWatchers().contains(player.getUniqueId())) continue;
+
+            if (flowStage.isPositionInBounds(new FlowPosition(x, y, z))) {
+                event.setCancelled(true);
+                player.getInventory().remove(event.getItemDrop().getItemStack());
+
+                flowStage.dropItem(event.getItemDrop().getItemStack().getType(),
+                        new FlowPosition(x, y, z));
             }
         }
     }
