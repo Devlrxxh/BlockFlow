4a3ab1b1418c28c6200fbac5e50a99ef3c3ad3ac
Devlrxxh <seabearvalorant@gmail.com>
2025-07-06 19:29:23 +0200

    chore: Improved EntityCache#getNearbyLivingEntities performance

--- src/main/java/dev/lrxh/blockFlow/entities/cache/EntityCache.java
+++ src/main/java/dev/lrxh/blockFlow/entities/cache/EntityCache.java
@@ -2,28 +2,48 @@ package dev.lrxh.blockFlow.entities.cache;
 
 import lombok.Getter;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Getter
 public class EntityCache {
-    private final Map<Integer, Entity> entities;
+    private Map<Integer, Entity> entities;
 
     public EntityCache() {
         this.entities = new HashMap<>();
     }
 
-    public List<LivingEntity> getNearbyLivingEntities(Location location, double radius) {
-        return entities.values().stream()
-                .filter(entity -> entity instanceof LivingEntity)
-                .filter(entity -> !entity.getLocation().equals(location))
-                .filter(entity -> entity.getLocation().distance(location) <= radius)
-                .map(entity -> (LivingEntity) entity)
-                .collect(Collectors.toList());
+    public List<Entity> getNearbyLivingEntities(Location location, double radius) {
+
+        double expander = 16.0D;
+
+        double x = location.getX();
+        double z = location.getZ();
+
+        int minX = (int) Math.floor((x - radius) / expander);
+        int maxX = (int) Math.floor((x + radius) / expander);
+
+        int minZ = (int) Math.floor((z - radius) / expander);
+        int maxZ = (int) Math.floor((z + radius) / expander);
+
+        World world = location.getWorld();
+
+        List<Entity> entities = new ArrayList<>();
+
+        for (int xVal = minX; xVal <= maxX; xVal++) {
+            for (int zVal = minZ; zVal <= maxZ; zVal++) {
+                if (world.isChunkLoaded(xVal, zVal)) {
+                    entities.addAll(Arrays.asList(world.getChunkAt(xVal, zVal).getEntities()));
+                }
+            }
+        }
+
+        entities.removeIf(entity -> entity.getLocation().distanceSquared(location) > radius * radius);
+
+        return entities;
     }
 }
